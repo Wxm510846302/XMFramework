f8872b14319fb679ba62c9b5b49d356d5e00224d
wxm <510846302@qq.com>
2025-06-25 22:01:33 +0800

    发布到cocoapods可以集成

--- XMFramework/XMSwiftSDK.swift
+++ XMFramework/XMSwiftSDK.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 @objc public class XMSwiftSDK: NSObject {
-
+    public override init() {
+        super.init()
+    }
+}
+class XMCtr:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.global().async {
+            // ❌ 错误：在后台线程启动动画会导致崩溃或未定义行为
+            UIView.animate(withDuration: 0.3) {
+                self.view.alpha = 0.5
+            }
+        }
+    }
 }
